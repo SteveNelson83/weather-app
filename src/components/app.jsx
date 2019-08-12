9f5623dad836c2211132325f4453479c76482e0b
SteveNelson83 <51002138+SteveNelson83@users.noreply.github.com>
2019-08-12 02:54:39 +0100

    component added

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -4,10 +4,10 @@ const App = () => <h1>`Hello ${props.name}!`</h1>;
 
 App.propTypes = {
     name: PropTypes.string,
-  };
-  
-  App.defaultProps = {
+};
+
+App.defaultProps = {
     name: 'World',
-  };
+};
 
 export default App;
