dae93f2ff678447942461be2ed4b457bf6b591f7
SteveNelson83 <51002138+SteveNelson83@users.noreply.github.com>
2019-08-12 02:21:20 +0100

    first commit(2)

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
 
-const App = () => <h1>Hello World</h1>;
+const App = () => <h1>`Hello ${props.name}!`</h1>;
+
+App.propTypes = {
+    name: PropTypes.string,
+  };
+  
+  App.defaultProps = {
+    name: 'World',
+  };
 
 export default App;
